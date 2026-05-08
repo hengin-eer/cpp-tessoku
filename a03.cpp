8e0548012ffdcef11f79ebdd2a96dba912be7cb1
timdaik <hirokix0608@gmail.com>
2026-05-09 00:23:16 +0900

    add: 簡単な全探索

---
+++ a03.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int N, K, P[109], Q[109];
+bool Answer = false;
+
+int main() {
+  cin >> N >> K;
+  for (int i = 1; i <= N; i++)
+    cin >> P[i];
+  for (int i = 1; i <= N; i++)
+    cin >> Q[i];
+
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= N; j++) {
+      if (P[i] + Q[j] == K)
+        Answer = true;
+    }
+  }
+
+  if (Answer == true)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  return 0;
+}
