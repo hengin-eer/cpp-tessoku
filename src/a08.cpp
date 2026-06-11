dbda03d41220a426e13d0a9d65f2961eb24103de
timdaik <hirokix0608@gmail.com>
2026-06-12 04:36:35 +0900

    feat: 2次元の累積和を実装

---
+++ src/a08.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int H, W, Q;
+  int X[1509][1509];
+  int Sum[1509][1509];
+  int A[100009], B[100009], C[100009], D[100009];
+  int Ans[100009];
+
+  cin >> H >> W;
+  for (int h = 1; h <= H; h++)
+    for (int w = 1; w <= W; w++)
+      cin >> X[h][w];
+
+  cin >> Q;
+  for (int q = 1; q <= Q; q++)
+    cin >> A[q] >> B[q] >> C[q] >> D[q];
+
+  // x, yの両軸で行ごと列ごとの累積和を計算する
+  Sum[0][0] = 0;
+  Sum[0][1] = 0;
+  Sum[1][0] = 0;
+  for (int h = 1; h <= H; h++)
+    for (int w = 1; w <= W; w++)
+      Sum[h][w] = Sum[h - 1][w - 1] + X[h][w];
+
+  for (int q = 1; q <= Q; q++)
+    Ans[q] = Sum[C[q]][D[q]] - Sum[A[q] - 1][D[q]] - Sum[C[q]][B[q] - 1] +
+             Sum[A[q] - 1][B[q] - 1];
+
+  for (int q = 1; q <= Q; q++)
+    cout << Ans[q] << "\n";
+
+  return 0;
+}
